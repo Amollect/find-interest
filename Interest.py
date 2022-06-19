abdf9c360ef46e298f37975534fb90e037db3f3d
Amol Shinde <107493995+Amollect@users.noreply.github.com>
2022-06-19 12:43:15 +0530

    Add files via upload

---
+++ Interest.py
@@ -0,0 +1,7 @@
+#find amount after adding interest
+
+base_a=float(input("enter the original amount:"))
+inte_a=float(input("enter interest on base amount"))
+amount=(base_a*inte_a)/100
+total=amount+base_a
+print("amount after adding interset in original amount:",total)
